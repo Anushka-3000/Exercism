8f89ec081b63226b3cc9acf4e90bed87c44230af
iamanushka <anushkadube1512@gmail.com>
2023-07-07 13:04:12 +0530

    Optimized code

--- two-fer/src/main/java/Twofer.java
+++ two-fer/src/main/java/Twofer.java
@@ -1,13 +1,5 @@
 public class Twofer {
     public String twofer(String name) {
-        if(name == null)
-        {
-            return "One for you, one for me.";
-        }
-        else
-        {
-            return "One for " + name + ", one for me.";
-        }
-
+        return String.format("One for %s, one for me.", name == null ? "you" : name);
     }
 }
