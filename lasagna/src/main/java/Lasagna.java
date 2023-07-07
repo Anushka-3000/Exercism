93d81b7efd2d6affc41cc8b4faf6cd10709f0625
iamanushka <anushkadube1512@gmail.com>
2023-07-07 13:00:06 +0530

    Add the static variable for prepTime

--- lasagna/src/main/java/Lasagna.java
+++ lasagna/src/main/java/Lasagna.java
@@ -1,13 +1,14 @@
 public class Lasagna {
+    static final int expectedTimeInOven = 40;
     // TODO: define the 'expectedMinutesInOven()' method
     public int expectedMinutesInOven()
     {
-        return 40;
+        return expectedTimeInOven;
     }
     // TODO: define the 'remainingMinutesInOven()' method
     public int remainingMinutesInOven(int actualTimeInOven)
     {
-        return (40 - actualTimeInOven);
+        return (expectedTimeInOven - actualTimeInOven);
     }
     // TODO: define the 'preparationTimeInMinutes()' method
     public int preparationTimeInMinutes(int numberOfLayers)
